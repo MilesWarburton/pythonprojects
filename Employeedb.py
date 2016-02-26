c64beb9d7e2472bd66f0996a6c8194e8205f28ad
Miles <miles.tristan.warburton@googlemail.com>
2016-02-26 20:15:36 +0000

    Added files via upload

--- Employeedb.py
+++ Employeedb.py
@@ -1,9 +1,19 @@
+import csv
+
 class Employeedb:
     def __init__(self):
         self.employeedict = {}
 
-    def addemployee(self, name, salary):
+
+    def addemployee(self, name, salary, occupation):
         self.employeedict[name] = float(salary)
+        with open('employeedb.csv', 'w') as csvfile:
+            fieldnames = ['first_name', 'salary', 'occupation']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'first_name': name, 'salary': float(salary), 'occupation': occupation})
+
+
 
 
     def avgsalary(self):
--- employeeClass.py
+++ employeeClass.py
@@ -6,7 +6,8 @@ newEmployee = Employeedb()
 while True:
     inputName = input("Employee Name: ")
     inputSalary = float(input("Employee Salary: "))
-    newEmployee.addemployee(inputName, inputSalary)
+    inputoccupation = input("What is Their Occupation?: ")
+    newEmployee.addemployee(inputName, inputSalary, inputoccupation)
     print(newEmployee.avgsalary())
     answer = input("Would you like to continue? Yes or No")
     if answer == "yes":
