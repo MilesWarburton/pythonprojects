898ba4c6f06decbdab8848b454f22b768e2d77ab
Miles <miles.tristan.warburton@googlemail.com>
2016-02-27 14:50:53 +0000

    Fixed CSV
    Headers now only append once after checking the file

--- Employeedb.py
+++ Employeedb.py
@@ -7,11 +7,17 @@ class Employeedb:
 
     def addemployee(self, name, salary, occupation):
         self.employeedict[name] = float(salary)
-        with open('employeedb.csv', 'w') as csvfile:
+        with open('employeedb.csv', 'a') as csvfile:
             fieldnames = ['first_name', 'salary', 'occupation']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerow({'first_name': name, 'salary': float(salary), 'occupation': occupation})
+
+            csvfilecount = csvfile.tell()
+            if csvfilecount == 0:
+                writer.writeheader()
+                writer.writerow({'first_name': name, 'salary': float(salary), 'occupation': occupation})
+            else:
+                writer.writerow({'first_name': name, 'salary': float(salary), 'occupation': occupation})
+            print(csvfile.tell())
 
 
 
